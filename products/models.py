778f7e7784a38e960dbc28e29e1411a35bf2d36f
crazdrms <dilipmaharjann@gmail.com>
2018-01-11 19:38:58 -0500

    absolute url

--- products/models.py
+++ products/models.py
@@ -48,6 +48,9 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return '{slug}'.format(slug=self.slug)
+
 
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
