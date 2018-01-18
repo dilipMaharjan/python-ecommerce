b2eadd8490d6b1a0f527cd2c34bb6b7cb12f80d4
crazdrms <dilipmaharjann@gmail.com>
2018-01-17 21:59:07 -0500

    user added for the cart

--- carts/views.py
+++ carts/views.py
@@ -3,21 +3,20 @@ from django.shortcuts import render
 from carts.models import Cart
 
 
-def create_cart():
-    return Cart.objects.create(user=None)
-
-
 def cart_home(request):
     cart_id = request.session.get('cart_id', None)
     if cart_id is None:
-        cart = create_cart()
+        cart = Cart.objects.new_session(user=request.user)
         request.session['cart_id'] = cart.id
         print("New Cart created")
     else:
         qs = Cart.objects.filter(id=cart_id)
         if qs.count() == 1:
             cart = qs.first()
+            if request.user.is_authenticated() and cart.user is None:
+                cart.user = request.user
+                cart.save()
         else:
-            cart = create_cart()
+            cart = Cart.objects.new_session(user=request.user)
             request.session['cart_id'] = cart.id
     return render(request, 'carts/home.html', {})
